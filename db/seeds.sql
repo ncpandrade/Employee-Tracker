16a1055d099cda9e64db0cb7d7718e372c94d20e
Nadine <nadine_paez@hotmail.com>
2021-11-14 19:55:10 -0500

    README

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,8 +6,8 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-  ('Manager', 80000.00, 1),
-  ('Associate', 50000, 2),
+  ('Marketing Analyst', 80000.00, 2),
+  ('Associate', 50000, 1),
   ('Engineer', 650000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
